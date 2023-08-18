dbd7831ecbfae7da265fb4cabee6ae86b6156640
Danilis567 <134603964+Danilis567@users.noreply.github.com>
2023-08-18 17:28:59 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const data = [
     title: "Test Data",
     explanation:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam explicabo, sunt mollitia quisquam quo quia, ",
-    img: "src/assets/TestProjectScreen.png",
+    img: "..src/assets/t.png",
     alt: "Test?",
     projectsLink: "https://loginscren.netlify.app/",
   },
